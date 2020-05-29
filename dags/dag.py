0088d1447f946778a1f122fa4d6039f27d53b3cd
João Marques <marques.joaopereira@gmail.com>
2020-05-29 18:52:40 +0100

    feat: update dag security context

--- dags/dag.py
+++ dags/dag.py
@@ -4,7 +4,6 @@ from datetime import datetime, timedelta
 from airflow.contrib.operators.kubernetes_pod_operator import KubernetesPodOperator
 from airflow.operators.dummy_operator import DummyOperator
 
-
 default_args = {
     'owner': 'airflow',
     'depends_on_past': False,
@@ -19,35 +18,40 @@ default_args = {
 dag = DAG(
     'kubernetes_sample', default_args=default_args, schedule_interval=timedelta(minutes=10))
 
-
 start = DummyOperator(task_id='run_this_first', dag=dag)
 
 passing = KubernetesPodOperator(namespace='test-airflow',
-                          image="python:3.6",
-                          cmds=["python","-c"],
-                          arguments=["print('hello world')"],
-                          labels={"foo": "bar"},
-                          name="passing-test",
-                          task_id="passing-task",
-                          get_logs=True,
-                          dag=dag
-                          )
+                                image="python:3.6",
+                                cmds=["python", "-c"],
+                                arguments=["print('hello world')"],
+                                labels={"foo": "bar"},
+                                name="passing-test",
+                                task_id="passing-task",
+                                get_logs=True,
+                                dag=dag,
+                                security_context={
+                                    "runAsUser": 1000,
+                                    "runAsGroup": 1000,
+                                    "fsGroup": 1000,
+                                    "fsGroupChangePolicy": "OnRootMismatch",
+                                }
+                                )
 
 failing = KubernetesPodOperator(namespace='test-airflow',
-                          image="ubuntu:16.04",
-                          cmds=["python","-c"],
-                          arguments=["print('hello world')"],
-                          labels={"foo": "bar"},
-                          name="fail",
-                          task_id="failing-task",
-                          get_logs=True,
-                          dag=dag
-                          )
+                                image="ubuntu:16.04",
+                                cmds=["python", "-c"],
+                                arguments=["print('hello world')"],
+                                labels={"foo": "bar"},
+                                name="fail",
+                                task_id="failing-task",
+                                get_logs=True,
+                                dag=dag
+                                )
 
 passing.set_upstream(start)
 failing.set_upstream(start)
 
-#helm install airflow \
+# helm install airflow \
 #     --set images.airflow.repository=my-dags \
 #     --set images.airflow.tag=0.0.2 \
 #     --set images.airflow.pullPolicy=Never \
