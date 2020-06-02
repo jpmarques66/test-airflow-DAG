fa61594d84187ac246aad9f3fd78c7da0f1839f0
João Marques <marques.joaopereira@gmail.com>
2020-06-02 11:20:35 +0100

    feat: improve dag

--- dags/dag.py
+++ dags/dag.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 #
 # Licensed to the Apache Software Foundation (ASF) under one
 # or more contributor license agreements.  See the NOTICE file
@@ -15,77 +16,61 @@
 # KIND, either express or implied.  See the License for the
 # specific language governing permissions and limitations
 # under the License.
-"""
-This is an example dag for using the Kubernetes Executor.
-"""
-import os
 
-from airflow import DAG
-from airflow.operators.python import PythonOperator
+from __future__ import print_function
+
+import time
+from builtins import range
+from pprint import pprint
+
 from airflow.utils.dates import days_ago
 
+from airflow.models import DAG
+from airflow.operators.python_operator import PythonOperator
+
 args = {
-    'owner': 'airflow',
-    'start_date': days_ago(2)
+    'owner': 'Airflow',
+    'start_date': days_ago(2),
 }
 
-with DAG(
-        dag_id='example_kubernetes_executor',
-        default_args=args,
-        schedule_interval=None,
-        tags=['example'],
-) as dag:
-    def print_stuff():  # pylint: disable=missing-docstring
-        print("stuff!")
-
-    def use_zip_binary():
-        """
-        Checks whether Zip is installed.
-        :return: True if it is installed, False if not.
-        :rtype: bool
-        """
-        return_code = os.system("zip")
-        if return_code != 0:
-            raise SystemError("The zip binary is missing")
+dag = DAG(
+    dag_id='example_python_operator',
+    default_args=args,
+    schedule_interval=None,
+    tags=['example']
+)
 
 
-    # You don't have to use any special KubernetesExecutor configuration if you don't want to
-    start_task = PythonOperator(
-        task_id="start_task",
-        python_callable=print_stuff
+# [START howto_operator_python]
+def print_context(ds, **kwargs):
+    pprint(kwargs)
+    print(ds)
+    return 'Whatever you return gets printed in the logs'
+
+
+run_this = PythonOperator(
+    task_id='print_the_context',
+    provide_context=True,
+    python_callable=print_context,
+    dag=dag,
+)
+# [END howto_operator_python]
+
+
+# [START howto_operator_python_kwargs]
+def my_sleeping_function(random_base):
+    """This is a function that will run within the DAG execution"""
+    time.sleep(random_base)
+
+
+# Generate 5 sleeping tasks, sleeping from 0.0 to 0.4 seconds respectively
+for i in range(5):
+    task = PythonOperator(
+        task_id='sleep_for_' + str(i),
+        python_callable=my_sleeping_function,
+        op_kwargs={'random_base': float(i) / 10},
+        dag=dag,
     )
 
-    # But you can if you want to
-    one_task = PythonOperator(
-        task_id="one_task",
-        python_callable=print_stuff,
-        executor_config={"KubernetesExecutor": {
-            "request_memory": "256Mi",
-            "limit_memory": "512Mi",
-            "image": "airflow/ci:latest"}}
-    )
-
-    # Use the zip binary, which is only found in this special docker image
-    two_task = PythonOperator(
-        task_id="two_task",
-        python_callable=use_zip_binary,
-        executor_config={"KubernetesExecutor": {"image": "airflow/ci_zip:latest"}}
-    )
-
-    # Limit resources on this operator/task with node affinity & tolerations
-    three_task = PythonOperator(
-        task_id="three_task",
-        python_callable=print_stuff,
-        executor_config={
-            "KubernetesExecutor": {"request_memory": "256Mi",
-                                   "limit_memory": "512Mi", }}
-    )
-
-    # Add arbitrary labels to worker pods
-    four_task = PythonOperator(
-        task_id="four_task",
-        python_callable=print_stuff,
-        executor_config={"KubernetesExecutor": {"labels": {"foo": "bar"}}}
-    )
-
-    start_task >> [one_task, two_task, three_task, four_task]
+    run_this >> task
+# [END howto_operator_python_kwargs]
